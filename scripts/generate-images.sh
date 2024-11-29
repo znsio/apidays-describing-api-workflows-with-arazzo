2c69aa7e0cf420144a95b780383f0234609d28c9
Frank Kilcommins <fkilcommins@gmail.com>
2024-11-29 17:26:14 +0000

    fix(script): use filename for image link

--- scripts/generate-images.sh
+++ scripts/generate-images.sh
@@ -48,7 +48,7 @@ find "$PRODUCTS_DIR" -type f -name "*.md" | while read -r markdown_file; do
 
             # Replace the Mermaid block with the image reference
             relative_path="./images/embedded/$(basename "$output_file")"
-            echo "![${sanitized_folder_name}-${block_counter}.png]($relative_path)" >> "$temp_file"
+            echo "![${current_file_name}]($relative_path)" >> "$temp_file"
         elif [[ $in_mermaid_block == true ]]; then
             # Inside a Mermaid block, collect content
             mermaid_content+="$line"$'\n'
